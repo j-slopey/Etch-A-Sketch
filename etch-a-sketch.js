6018a3caa4b5dc4f91a80a13f3b9993e73c2b323
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-13 11:03:16 -0600

    Working etch-a-sketch for set grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,26 @@
 
 const boardSize = 16;
 const board = document.querySelector("#board");
-const row = document.createElement("div");
-const square = document.createElement("div");
-row.classList.add("row");
-square.classList.add("square");
+const rowModel = document.createElement("div");
+const squareModel = document.createElement("div");
+rowModel.classList.add("row");
+squareModel.classList.add("square");
+
 for(let i = 0; i < boardSize; i++){
-    row.appendChild(square.cloneNode(true))
+
+    rowModel.appendChild(squareModel.cloneNode(true));
 }
 
 for(let i = 0; i < boardSize; i++){
-    board.appendChild(row.cloneNode(true))
+
+    board.appendChild(rowModel.cloneNode(true));
 }
+
+const squares = document.querySelectorAll(".square");
+squares.forEach( (square) => {
+    square.addEventListener("mouseover",(e) => e.target.style.background = "black" );
+})
+
+const resetButton = document.querySelector("#clear");
+resetButton.addEventListener("click", () => squares.forEach((square) => square.style.background = "transparent"));
+
